6c8f71e259c233f7ed203ec4c04075c4de467939
Hugo Souza <hsouza@magnasistemas.com.br>
2019-07-05 14:54:21 -0300

    TypeScrip parte 2. NameSpace e módulos externos. Para instalar o lite-server: 'npm install lite-server@2.3.0 --save-dev'. Para intalar o concurrently: 'npm install concurrently@3.4.0 --save-dev'. Instalado os dois apenas executar o 'npm start'

---
+++ alurabank/app/js/models/index.js
@@ -0,0 +1,20 @@
+System.register(["./Negociacao", "./Negociacoes"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (Negociacao_1_1) {
+                exports_1({
+                    "Negociacao": Negociacao_1_1["Negociacao"]
+                });
+            },
+            function (Negociacoes_1_1) {
+                exports_1({
+                    "Negociacoes": Negociacoes_1_1["Negociacoes"]
+                });
+            }
+        ],
+        execute: function () {
+        }
+    };
+});
---
+++ alurabank/app/js/views/index.js
@@ -0,0 +1,25 @@
+System.register(["./View", "./MensagemView", "./NegociacoesView"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (View_1_1) {
+                exports_1({
+                    "View": View_1_1["View"]
+                });
+            },
+            function (MensagemView_1_1) {
+                exports_1({
+                    "MensagemView": MensagemView_1_1["MensagemView"]
+                });
+            },
+            function (NegociacoesView_1_1) {
+                exports_1({
+                    "NegociacoesView": NegociacoesView_1_1["NegociacoesView"]
+                });
+            }
+        ],
+        execute: function () {
+        }
+    };
+});
